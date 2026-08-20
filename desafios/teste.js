0d596d635477cb7a499629b225c6f899cf326c4c
felipeRdrgs <feliperdf505@gmail.com>
2026-08-20 11:26:19 -0300

    pessoal

--- desafios/teste.js
+++ desafios/teste.js
@@ -1,15 +1,42 @@
 const readlinesync = require('readline-sync');
 
-let filmes = [
-    { titulo: "homem aranha", classificação: 14},
-    { titulo: "toy story 5", classificação: 6},
-    { titulo: "todo mundo em panico", classificação: 18}
+const name = readlinesync.question(`digite seu nome:`);
+console.log(`=== SEJA BEM VINDO ${name} `)
+const generoJogos = readlinesync.questionInt(`qual e o tipo de jogos que você procura jogar? \n1- acão \n2- aventura \n3- violencia \n4- suspense \n5- terror \n6- corrida \n7-esportes`)
+
+if (generoJogos == 1)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let games = [
+    { titulo: "fortnite", genero: "ação", lassificação: 14},
+    { titulo: "roblox", genero: "aventura", classificação: 8},
+    { titulo: "free fire", genero: "ação e violencia", classificação: 14},
+    { titulo: "GTA V", genero: "ação, violencia e aventura", classificação: 18},
+    { titulo: "forza horizon", genero: "esportes e corrida", classificação: 10},
+    { titulo: "EAFC 26", genero: "esportes", classificação: 8}
 ];
     
 let idade = readlinesync.questionInt(`informe sua idade.`)
-for (let filme of filmes) {
-    if (idade >= filme.classificação) {
-        console.log(filme.titulo);
+console.log(`=== LISTA DE JOGOS QUE VOCÊ PODE JOGAR ===`)
+for (let jogo of games) {
+    if (idade >= jogo.classificação) {
+        console.log(jogo.titulo);
     }
 }
 
